2a1270dc328f012bd978d3f7073a14058bd0c7a6
HolmCramer <50287469+HolmCramer@users.noreply.github.com>
2024-10-24 22:40:21 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,5 @@
 from game import Game
 
-
-main = Game()
-main.play()
-# main.gamestate.move("D2 B5")
-# test = main.gamestate.inputConv("B2")
-# print(test)
-# main.gamestate.move("D2 to D3")
-# main.gamestate.move("D3D4")
-# main.gamestate.move("B1 to C3")
-# print(main.gamestate.inputConv("D2"))
-# print(main.gamestate.inputConv("D3"))
-# print(main.gamestate.gamestate[3][6])
-# print(main.gamestate.gamestate[3][5])
-# main.gui.updateGUI(main.gamestate.gamestate)
-# print(type(main.gamestate.gamestate))
-# main.play()
-#print(main.gamestate.gamestate)
+if "__name__" == "__main__:
+  main = Game()
+  main.play()
