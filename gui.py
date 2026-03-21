94c99ec4c75e4d9eddc08913b509e60d950a0c59
Holm Cramer <holmcramer@gmail.com>
2026-03-21 19:38:42 +0100

    refactor drawBoard function

--- gui.py
+++ gui.py
@@ -9,61 +9,79 @@ class GUI:
         self.board = Board()
 
     def drawBoard(self, gamestate: Gamestate) -> None:
-        print("\033[H\033[J", end="")
-        print(" " + 6 * self.board.width * "_", end="")
-        print()
+        self.clear_terminal()
+        self.draw_top_boarder()
         for y in range(self.board.height):
             for lines in range(3):
                 if lines == 0:
-                    print("|", end="")
-                    for x in range(self.board.width):
-                        square = self.board.squares[x][y]
-                        if square is not None and square.color == COLOR.WHITE:
-                            print(6 * Square.whiteASCII, end="")
-                        else:
-                            print(6 * Square.blackASCII, end="")
-                    print("|", end="")
-                    print()
+                    self.print_top(y)
                 if lines == 1:
-                    print("|", end="")
-                    for x in range(self.board.width):
-                        square = self.board.squares[x][y]
-                        piece = (
-                            gamestate.gamestate[x][y]
-                            if gamestate.gamestate[x][y] is not None
-                            else None
-                        )
-
-                        bg = (
-                            Square.whiteASCII
-                            if square and square.color == COLOR.WHITE
-                            else Square.blackASCII
-                        )
-
-                        print(2 * bg, end="")
-
-                        if piece:
-                            print(piece.icon + " ", end="")
-                        else:
-                            print(2 * bg, end="")
-
-                        print(2 * bg, end="")
-                    print("|", end="")
-                    print()
+                    self.print_mid(y, gamestate)
                 if lines == 2:
-                    print("|", end="")
-                    for x in range(self.board.width):
-                        square = self.board.squares[x][y]
-                        if square is not None and square.color == COLOR.WHITE:
-                            print(self.board.coords[y][x], end="")
-                            print(4 * Square.whiteASCII, end="")
-                        else:
-                            print(self.board.coords[y][x], end="")
-                            print(4 * Square.blackASCII, end="")
-                    print("|", end="")
-                    print()
+                    self.print_bottom(y)
+        self.draw_bottom_boarder()
+
+    def print_top(self, y) -> None:
+        print("|", end="")
+        for x in range(self.board.width):
+            square = self.board.squares[x][y]
+            if square is not None and square.color == COLOR.WHITE:
+                print(6 * Square.whiteASCII, end="")
+            else:
+                print(6 * Square.blackASCII, end="")
+        print("|", end="")
+        print()
+
+    def print_mid(self, y, gamestate) -> None:
+        print("|", end="")
+        for x in range(self.board.width):
+            square = self.board.squares[x][y]
+            piece = (
+                gamestate.gamestate[x][y]
+                if gamestate.gamestate[x][y] is not None
+                else None
+            )
+
+            bg = (
+                Square.whiteASCII
+                if square and square.color == COLOR.WHITE
+                else Square.blackASCII
+            )
+
+            print(2 * bg, end="")
+
+            if piece:
+                print(piece.icon + " ", end="")
+            else:
+                print(2 * bg, end="")
+
+            print(2 * bg, end="")
+        print("|", end="")
+        print()
+
+    def print_bottom(self, y) -> None:
+        print("|", end="")
+        for x in range(self.board.width):
+            square = self.board.squares[x][y]
+            if square is not None and square.color == COLOR.WHITE:
+                print(self.board.coords[y][x], end="")
+                print(4 * Square.whiteASCII, end="")
+            else:
+                print(self.board.coords[y][x], end="")
+                print(4 * Square.blackASCII, end="")
+        print("|", end="")
+        print()
+
+    def draw_top_boarder(self) -> None:
+        print(" " + 6 * self.board.width * "_", end="")
+        print()
+
+    def draw_bottom_boarder(self) -> None:
         print(" " + 6 * self.board.width * "¯", end="")
         print()
+
+    def clear_terminal(self) -> None:
+        print("\033[H\033[J", end="")
 
     def enterMove(self) -> str:
         moveInput = ""
